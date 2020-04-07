38c0bda7139fe574b322f4e03f6c3670b470bded
Alex-SK <aleksey.kozyura@gmail.com>
2020-04-07 18:25:31 +1000

    Add guides creating

--- scripts/auth.js
+++ scripts/auth.js
@@ -12,6 +12,21 @@ auth.onAuthStateChanged(user => {
     }
 });
 
+// create new guide
+const createForm = document.querySelector('#create-form');
+createForm.addEventListener('submit', e => {
+    e.preventDefault();
+    db.collection('guides').add({
+        title: createForm['title'].value,
+        content: createForm['content'].value
+    }).then(() => {
+        M.Modal.getInstance(document.querySelector('#modal-create')).close();
+        createForm.reset();
+    }).catch(err => console.log(err));
+    
+
+});
+
 // signup
 const signupForm = document.querySelector('#signup-form');
 signupForm.addEventListener('submit', e => {
