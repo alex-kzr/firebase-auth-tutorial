40c229fda7fb64e3a448568dbf698dd5aa9a5f94
Alex-SK <aleksey.kozyura@gmail.com>
2020-04-08 14:03:18 +1000

    Add & deploy addAdminRole firebase function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,19 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+exports.addAdminRole = functions.https.onCall((data, context) => {
+    // get user and add custom claim (admin)
+    return admin.auth().getUserByEmail(data.email).then(user => {
+        return admin.auth().setCustomUserClaims(usder.uid, {
+            admin: true
+        });
+    }).then(() => {
+        return {
+            message: `Success! ${data.email} has been made an admin`
+        };
+    }).catch(err => {
+        return err;
+    });
+});
